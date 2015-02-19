e050457a894192fb49ec68731ed42181ba940197
Sebastian Brandes Kraaijenzank <sb@sebastianbrandes.com>
2015-02-19 14:24:18 +0100

    Init

---
+++ src/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Mvc;
+using WebApplication4.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var person = new Person();
+            person.Name = "Sebastian";
+            person.AgeInYears = 55;
+
+            return View(person);
+        }
+    }
+}
---
+++ src/WebApplication4/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WebApplication4.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int AgeInYears { get; set; }
+    }
+}
